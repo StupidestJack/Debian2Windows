6ea07d0921ab1fffc9a8f649385fdb2bb01204b0
Niugnep Hsueh <penguin@niugnep.idv.tw>
2026-08-22 11:33:08 +0800

    加入gpg的actions

--- genindex.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# 產生 Packages 索引檔
-dpkg-scanpackages --multiversion debs /dev/null > Packages
-
-# 壓縮索引檔（APT 優先讀取壓縮檔）
-gzip -k -f Packages
-
-# 生成簡單的 Release 檔案
-apt-ftparchive release . > Release
